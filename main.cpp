2c3c586d1ff940c721363fbbbd24038175dcbfd6
potomnazovy <skeleton39876@mail.ru>
2026-04-06 12:12:44 +0300

    micro cg fix

--- main.cpp
+++ main.cpp
@@ -676,7 +676,7 @@ bool testPushBackRangeFromVector()
 
 bool testPushBackRangeGrow()
 {
-  topit::Vector<int> v{2, 5};
+  topit::Vector< int > v{2, 5};
   int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   
   v.pushBackRange(arr, 10);
